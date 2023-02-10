3daf1772f35fdfe86e3a103625cb0fd390ff5117
ssenarath <97403972+SSenarath@users.noreply.github.com>
2023-02-09 23:43:52 -0500

    Paraphrase personal bio

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,9 +9,7 @@ export default function Hero(){
             <div className="container">
                 <div className="hero-left-container">
                     <h1 className="main-intro">Hello, I'm Sanduni | <br/> Junior Developer </h1>
-                    <p className="about-me p-lineheight" >As a former engineering student and project manager who always ended up being the tech guru of
-                        a team, it became obvious that I should make a change. While it wasn't the easiest decision, I am now happily pursuing my passions and am learning to code through online resources to create applications to solve real world
-                        problems for the people around me.</p>
+                    <p className="about-me p-lineheight" >As a former engineering student and project manager who always ended up being the tech support person on a team, it became   obvious that I should make a change. While it wasn't the easiest decision, I am now happily pursuing my passions and am learning to code through online resources to create practical solutions for real-world problems and make a difference for those around me.</p>
                     <div className ="links">
                         <a className='btn'><Link  activeClass="active" to="skills" spy={true} smooth={true}>Skills</Link></a>
                         <a className='btn'><Link  activeClass="active" to="projects" spy={true} smooth={true}>Projects</Link></a>
