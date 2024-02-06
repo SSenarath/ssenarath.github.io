16238dac4776ebac9765c97822d9b45edf46de59
ssenarath <97403972+SSenarath@users.noreply.github.com>
2024-02-06 03:31:56 -0500

    update portfolio website

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,16 +8,18 @@ export default function Hero() {
       <div className="container">
         <div className="hero-left-container">
           <h1 className="main-intro">
-            Hello, I'm Sanduni | <br /> Junior Developer{" "}
+            Sanduni Senarath | <br /> Junior Developer{" "}
           </h1>
           <p className="about-me p-lineheight">
             Hello there! I'm Sanduni, a Front-End Software Developer
-            specializing in crafting applications from scratch using Nuxt. In my
-            previous career, I was a construction project manager, that was
-            frequently stepping into the role of the team's go-to tech support.
-            Realizing the call for change, I happily transitioned into pursuing
-            my passion for coding Now, I am actively learning through online
-            resources to create practical solutions for real-world challenges.
+            specializing in crafting applications from scratch using JavaScript,
+            HTML, and CSS. In my previous career, I was a construction project
+            manager that was frequently stepping into the role of the team's
+            go-to tech support. With the help of an internship that turned into
+            a full-time Software Developer position at Arudi, I happily
+            transitioned into pursuing my passion for coding. Now, I am actively
+            learning through online resources to create practical solutions for
+            real-world challenges.
           </p>
           <div className="links">
             <a className="btn">
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,51 +7,41 @@ export default function About(){
                     <h1 className="section-title">MY SKILLS & TOOLS</h1>
                     <div className="skill-category">
                         <h3>Languages: </h3>
-                        <p>Node.js</p>
+                        <p>Javascript</p>
                         <p>|</p>
-                        <p>JavaScript</p>
+                        <p>HTML</p>
                         <p>|</p>
-                        <p>Java</p>
+                        <p>CSS</p>
                         <p>|</p>
-                        <p>Python </p>
-                    </div>
-                    <div className="skill-category">
-                        <h3>Frameworks: </h3>
-                        <p>ReactJS</p>
+                        <p>React</p>
                         <p>|</p>
-                        <p>Vue.js</p>
+                        <p>Vue</p>
+                        <p>|</p>
+                        <p>Nuxt</p>
+                        <p>|</p>
+                        <p>Element-UI</p>
                         <p>|</p>
                         <p>Tailwind CSS</p>
                         <p>|</p>
-                        <p>ExpressJS</p>
+                        <p>FastAPI</p>
                         <p>|</p>
-                        <p>Mongoose</p>
+                        <p>Python</p>
                     </div>
                     <div className="skill-category">
-                        <h3>Databases: </h3>
-                        <p>MongoDB</p>
-                    </div>
-                    <div className="skill-category dev">
-                        <h3>Developer Tools: </h3>
-                            <div className="dev-tools">
-                                <p>HTML</p>
-                                <p>|</p>
-                                <p>CSS</p>
-                                <p>|</p>
-                                <p>Git</p>
-                                <p>|</p>
-                                <p>Github</p>
-                                <p>|</p>
-                                <p>Visual Studio Code</p>
-                                <p>|</p>
-                                <p>Netbeans</p>
-                                <p>|</p>
-                                <p>Chrome DevTools</p>
-                                <p>|</p>
-                                <p>Linux (Ubuntu)</p>
-                                <p>|</p>
-                                <p>Figma</p>
-                            </div>
+                        <h3>Frameworks: </h3>
+                        <p>Git</p>
+                        <p>|</p>
+                        <p>GitHub</p>
+                        <p>|</p>
+                        <p>Azure DevOps</p>
+                        <p>|</p>
+                        <p>Jira</p>
+                        <p>|</p>
+                        <p>Chrome</p>
+                        <p>|</p>
+                        <p>DevTools</p>
+                        <p>|</p>
+                        <p>Figma</p>
                     </div>
             </div>
         </div>
