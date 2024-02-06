1cf41ff567edccd1dd54780f9fdfd84eecd10e7b
ssenarath <97403972+SSenarath@users.noreply.github.com>
2024-02-06 01:46:24 -0500

    Revert "Revert "Add TaiwindCSS and Vue.js to skills and tools section""
    This reverts commit 7b52bf6502b3d384c0caf203cb1de11e57b94356.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,56 +1,31 @@
 import React from "react";
 
-export default function About(){
-    return(
-      <div>
-        <div id ="skills">
-            <div className="container">
-                    <h1 className="section-title">MY SKILLS & TOOLS</h1>
-                    <div className="skill-category">
-                        <h3>Languages: </h3>
-                        <p>Node.js</p>
-                        <p>|</p>
-                        <p>JavaScript</p>
-                        <p>|</p>
-                        <p>Java</p>
-                        <p>|</p>
-                        <p>Python </p>
-                    </div>
-                    <div className="skill-category">
-                        <h3>Frameworks: </h3>
-                        <p>ReactJS</p>
-                        <p>|</p>
-                        <p>ExpressJS</p>
-                        <p>|</p>
-                        <p>Mongoose</p>
-                    </div>
-                    <div className="skill-category">
-                        <h3>Databases: </h3>
-                        <p>MongoDB</p>
-                    </div>
-                    <div className="skill-category dev">
-                        <h3>Developer Tools: </h3>
-                            <div className="dev-tools">
-                                <p>HTML</p>
-                                <p>|</p>
-                                <p>CSS</p>
-                                <p>|</p>
-                                <p>Git</p>
-                                <p>|</p>
-                                <p>Github</p>
-                                <p>|</p>
-                                <p>Visual Studio Code</p>
-                                <p>|</p>
-                                <p>Netbeans</p>
-                                <p>|</p>
-                                <p>Chrome DevTools</p>
-                                <p>|</p>
-                                <p>Linux (Ubuntu)</p>
-                                <p>|</p>
-                                <p>Figma</p>
-                            </div>
-                    </div>
-            </div>
+export default function About() {
+  return (
+    <div id="skills">
+      <div className="container ">
+        <h1 className="section-title">MY SKILLS & TOOLS</h1>
+        <div className="skill-category flex items-start">
+          <h3 className="font-bold">Programming Skills: </h3>
+          <div className="flex gap-2 flex-wrap">
+            <p>Javascript</p>
+            <p>|</p>
+            <p>HTML</p>
+            <p>|</p>
+            <p>React</p>
+            <p>|</p>
+            <p>Vue</p>
+            <p>|</p>
+            <p>Nuxt</p>
+            <p>|</p>
+            <p>Element-UI</p>
+            <p>|</p>
+            <p>Tailwind CSS</p>
+            <p>|</p>
+            <p>FastAPI</p>
+            <p>|</p>
+            <p>Python</p>
+          </div>
         </div>
         <div className="skill-category flex items-start">
           <h3 className="font-bold">Other: </h3>
