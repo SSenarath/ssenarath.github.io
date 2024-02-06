b36d14463a52bab713ce132d676d9e2db2d121ef
ssenarath <97403972+SSenarath@users.noreply.github.com>
2024-02-06 00:25:50 -0500

    update portfolio website

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,23 +1,27 @@
 import React from "react";
 import Toolkit from "./Toolkit.js";
 
-export default function ProjectCard({item}){
-    return (
-        <div className="projectcard">
-    
-            <a target="_blank" href={item.websiteLink}><img className="project-img" src={require(`../images/${item.coverImg}`)} alt="Sanduni Senarath in a white blouse smiling"/></a>  
-            
-            <div className="projectcard-right-info">
-                <h2>{item.title}</h2>
-                <Toolkit project={item.tools} key={item.id}/>
-                <p className="p-lineheight">{item.description}</p>
-                <div className="links">
-                    <a className = "btn" target="_blank" href={item.websiteLink}>Live Site</a>
-                    <a className = "btn" target="_blank" href={item.githubRepoLink}>Github Repository</a>
-                </div>
-                    
-            </div>
+export default function ProjectCard({ item }) {
+  return (
+    <div className="border flex bg-[#e9e9e9]">
+      <div className="w-1/4 p-2 flex items-center">
+        <a target="_blank" href={item.websiteLink}>
+          <img src={require(`../images/${item.coverImg}`)} alt="" />
+        </a>
+      </div>
+      <div className="w-3/4 flex flex-col p-2 justify-center item">
+        <h2>{item.title}</h2>
+        <Toolkit project={item.tools} key={item.id} />
+        <p className="p-lineheight">{item.description}</p>
+        <div className="links">
+          <a className="btn" target="_blank" href={item.websiteLink}>
+            Live Site
+          </a>
+          <a className="btn" target="_blank" href={item.githubRepoLink}>
+            Github Repository
+          </a>
         </div>
-
-    )
-}  
+      </div>
+    </div>
+  );
+}
--- src/components/data.js
+++ src/components/data.js
@@ -1,75 +1,63 @@
 export default {
-    generalTools: [
-        "HTML5",
-        "CSS5",
+  generalTools: [
+    "HTML5",
+    "CSS5",
+    "JavaScript",
+    "jQuery",
+    "React",
+    "Git",
+    "Github",
+    "VS Code",
+    "Figma",
+    "Node.js",
+  ],
+  projectinfo: [
+    {
+      id: 1,
+      title: "Restaurant Management App",
+      description: `At the beginning of my coding journey, I developed a restaurant management app with my sister using Node.js that can process orders, track order history and store product details to fulfill the business need of my mom’s small catering business. Back then, lacking experience in front-end frameworks, the code wasn't as streamlined as I would have preferred. Recently, I started re-creating the application using my experience with Nuxt, Element UI and Tailwind CSS.  While the application remains a work in progress, you can explore the current status below. Please note that the demo version excludes real ingredient information (family secrets, you know!)`,
+      coverImg: "restaurant.png",
+      websiteLink: "https://main--luneil-cafe-management-app.netlify.app/",
+      githubRepoLink: "https://github.com/SSenarath/luneil-cafe-demo-frontend-v2",
+      tools: [
+        "Node.js",
+        "ExpressJS",
+        "Mongoose",
+        "MongoDB",
         "JavaScript",
-        "jQuery",
-        "React",
-        "Git",
-        'Github',
-        "VS Code",
-        "Figma",
-        "Node.js"
-    ],
-    projectinfo: [
-        {
-            id: 1,
-            title: "Restaurant Management App",
-            description: `My sister and I wanted to collaborate on a project, so we chose to developed a restaurant management
-            system using Node.js that can process orders, track order history and store product details to fulfill the
-            business need of my mom’s small catering business. She is using her own version with the real recipes (because we can’t give away family secrets 😉) but you can checkout the demo version below.`,
-            coverImg: "restaurant.png",
-            websiteLink: "https://luneil-cafe-demo.up.railway.app/",
-            githubRepoLink: "https://github.com/HSenarath/luneil-cafe-demo",
-            tools: [
-                "Node.js",
-                "ExpressJS",
-                "Mongoose",
-                "MongoDB",
-                "JavaScript",
-                "HTML",
-                "CSS"
-            ]
-        }
-        ,
-        {
-            id: 2,
-            title: "Calculator",
-            description: `This calculator was the final project of the foundations path of an online resource known as The Odin
-            Project. One thing I really appreciate about this resource is that it takes a very hands-off approach
-            meaning that all they do is simply give us an idea for the project and from that point on its our
-            responsibility to figure out the logic behind the code, styles and to find documentation that might help
-            us implement new methods and functions. Through this project, I was able to practice my front end
-            development skills.`,
-            coverImg: "calculator.png",
-            websiteLink: "https://ssenarath.github.io/calculator/",
-            githubRepoLink: "https://github.com/SSenarath/calculator",
-            tools: [
-                "JavaScript",
-                "HTML",
-                "CSS"
-            ]
-        }
-        ,
-        {
-            id: 3,
-            title: "Colour Scheme Generator",
-            description: `This Color pallete generator that I created to practice fetching from APIs ended up becoming one of my
+        "HTML",
+        "CSS",
+      ],
+    },
+    ,
+    // {
+    //     id: 2,
+    //     title: "Calculator",
+    //     description: `This calculator was the final project of the foundations path of an online resource known as The Odin
+    //     Project. One thing I really appreciate about this resource is that it takes a very hands-off approach
+    //     meaning that all they do is simply give us an idea for the project and from that point on its our
+    //     responsibility to figure out the logic behind the code, styles and to find documentation that might help
+    //     us implement new methods and functions. Through this project, I was able to practice my front end
+    //     development skills.`,
+    //     coverImg: "calculator.png",
+    //     websiteLink: "https://ssenarath.github.io/calculator/",
+    //     githubRepoLink: "https://github.com/SSenarath/calculator",
+    //     tools: [
+    //         "JavaScript",
+    //         "HTML",
+    //         "CSS"
+    //     ]
+    // }
+    {
+      id: 3,
+      title: "Colour Scheme Generator",
+      description: `This Color pallete generator that I created to practice fetching from APIs ended up becoming one of my
             most revisited sites as I use it to often when implenting new projects. This project idea was suggested to
             me by Scrimba, another online resource that has become invaluable in my coding journey`,
-            coverImg: "color-scheme.png",
-            websiteLink: "https://ssenarath.github.io/ColorSchemeGenerator/",
-            githubRepoLink: "https://github.com/SSenarath/ColorSchemeGenerator",
-            tools: [
-                "JavaScript",
-                "HTML",
-                "CSS"
-            ]
-        }
-    ]
-
-
-}
-
-
-
+      coverImg: "color-scheme.png",
+      websiteLink: "https://ssenarath.github.io/ColorSchemeGenerator/",
+      githubRepoLink: "https://github.com/SSenarath/ColorSchemeGenerator",
+      tools: ["JavaScript", "HTML", "CSS"],
+    },
+  ],
+};
